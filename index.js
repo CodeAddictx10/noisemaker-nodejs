fe9ceb8ea62e551947861d8e77d6ba95ce9e026c
olufemi-samuel <onadeji.samuel@yahoo.com>
2021-11-08 19:49:04 +0100

    More files to cloudinary

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
+const path = require("path");
 const app = express();
-app.use(fileUpload());
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    }),
+);
 const port = 5000;
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // app.use(express.urlencoded({ bodyParser: true })); //this will through warning body-parser deprecated undefined extended: provide extended option
@@ -47,19 +61,37 @@ app.get("/rem/:id", (req, res) => {
     noiselist.splice(index, 1);
     res.redirect("/");
 });
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
+    //async makes the image show on add else it would takes longer
     if (req.files) {
         let file = req.files.photo;
-        file.mv(`./uploads/${file.name}`, (err) => {
-            if (err) {
-                console.log("An error has occured");
-            } else {
-                let newName = { name: req.body.name, pic: file.name, times: 1 };
-                console.log(newName);
-                noiselist.push(newName);
-            }
-        });
+
+        // save image to cloudinary
+        try {
+            let res = await cloudinary.uploader.upload(file.tempFilePath, {
+                folder: "nodejs",
+            });
+            let newName = {
+                name: req.body.name,
+                pic: res.secure_url,
+                times: 1,
+            };
+            noiselist.push(newName);
+            console.log("success");
+        } catch (error) {
+            console.log("An error has occured");
+        }
+
+        // Save image to local directory
+        // file.mv(` ${path.join(__dirname, "uploads")}/${file.name}`, (err) => {
+        //     if (err) {
+        //         console.log("An error has occured");
+        //     } else {
+        //         let newName = { name: req.body.name, pic: file.name, times: 1 };
+        //         console.log(newName);
+        //         noiselist.push(newName);
+        //     }
+        // });
     }
-    console.log(req.files);
     res.redirect("/");
 });
